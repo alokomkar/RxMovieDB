61a0fd018336fb9f96a1ed1169acf82aa1606268
Alok Omkar <alokomkar.gudikote@gmail.com>
2017-06-15 15:51:14 +0530

    Corrected error

--- mobile/src/main/java/com/alokomkar/rxmoviedb/movielist/MoviesResponse.java
+++ mobile/src/main/java/com/alokomkar/rxmoviedb/movielist/MoviesResponse.java
@@ -14,7 +14,7 @@ import java.util.List;
  * Created by rajkiran on 12/06/17.
  */
 @JsonObject
-public class MoviesDetailsResponse {
+public class MoviesResponse {
     @JsonField(name = "page")
     private int page;
     @JsonField(name = "results")
