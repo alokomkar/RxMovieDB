7d4b7ce15f87ec04398294da8bd65fbe322b98d0
Binay <binay.rana@cognitiveclouds.com>
2017-06-20 10:53:45 +0530

    changes in navigation drawer

--- mobile/src/main/java/com/alokomkar/rxmoviedb/MainActivity.java
+++ mobile/src/main/java/com/alokomkar/rxmoviedb/MainActivity.java
@@ -252,13 +252,13 @@ public class MainActivity extends AppCompatActivity implements NavigationListene
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_trailer) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_theater) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_favorites) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_message) {
 
         }
 
